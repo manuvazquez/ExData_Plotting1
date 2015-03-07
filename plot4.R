fa196a4a03cd5f4981ef4889fbdd3483a2909978
Manuel A. Vázquez <manuavazquez@gmail.com>
2015-03-07 11:41:34 +0100

    - the data is downloaded and unzipped at the beginning of each script

--- plot4.R
+++ plot4.R
@@ -1,3 +1,12 @@
+# URL to the "zip" file
+fileURL <- 'http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+
+# downloading
+download.file(fileURL,destfile='household_power_consumption.zip')
+
+# file must be unzipped
+unzip('household_power_consumption.zip')
+
 # data file name
 file <- 'household_power_consumption.txt'
 
